5d731c7c914a176f2ffbfde3594ea39c4d38b0be
kimyubi <ieieie0419@gmail.com>
2022-11-15 02:39:16 +0900

    REFACTOR: Validation 처리를 위한 BaseResponse, ExceptionAdvisor 리팩토링

--- 타미/RestfulService/src/main/java/com/example/demo/config/BaseResponse.java
+++ 타미/RestfulService/src/main/java/com/example/demo/config/BaseResponse.java
@@ -33,5 +33,12 @@ public class BaseResponse<T> {//BaseResponse 객체를 사용할때 성공, 실�
         this.message = status.getMessage();
         this.code = status.getCode();
     }
+
+    // Validation
+    public BaseResponse(T result, String message, int code) {
+        this.isSuccess = false;
+        this.message = message;
+        this.code = code;
+    }
 }
 
--- 타미/RestfulService/src/main/java/com/example/demo/config/ExceptionAdvisor.java
+++ 타미/RestfulService/src/main/java/com/example/demo/config/ExceptionAdvisor.java
@@ -1,12 +1,15 @@
 package com.example.demo.config;
 
+import com.example.demo.config.BaseResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionAdvisor {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -26,6 +29,6 @@ public class ExceptionAdvisor {
             builder.append("]");
         }
 
-        return new BaseResponse<>(builder.toString());
+        return new BaseResponse("유효성 검사 에러",builder.toString(), HttpStatus.BAD_REQUEST.value());
     }
 }
